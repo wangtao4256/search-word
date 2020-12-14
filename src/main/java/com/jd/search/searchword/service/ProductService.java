bc4eb160198b019d1c603911c827e24615a15e14
wangtao <wangtao_4256@163.com>
2020-12-14 19:31:33 +0800

    分词代码实现

--- src/main/java/com/jd/search/searchword/service/ProductService.java
+++ src/main/java/com/jd/search/searchword/service/ProductService.java
@@ -12,6 +12,7 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilder;
@@ -19,12 +20,15 @@ import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -56,6 +60,13 @@ public class ProductService {
         TermQueryBuilder termQuery = new TermQueryBuilder("productName", keyword);
         searchSourceBuilder.query(termQuery);
 
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("productName");
+        highlightBuilder.preTags("<span style='color:red'>");
+        highlightBuilder.requireFieldMatch(false);
+        highlightBuilder.postTags("</span>");
+        searchSourceBuilder.highlighter(highlightBuilder);
+
         request.source(searchSourceBuilder);
         SearchResponse searchResponse = restHighLevelClient.search(request, RequestOptions.DEFAULT);
         SearchHits hits = searchResponse.getHits();
@@ -65,6 +76,16 @@ public class ProductService {
             SearchHit product = iterator.next();
 
             Product search = JSON.parseObject(product.getSourceAsString(), Product.class);
+            Map<String, HighlightField> highlightFields = product.getHighlightFields();
+            HighlightField productNameHigh = highlightFields.get("productName");
+            StringBuilder builder = new StringBuilder();
+            if (null != productNameHigh) {
+                Text[] fragments = productNameHigh.fragments();
+                for (Text fragment : fragments) {
+                    builder.append(fragment);
+                }
+            }
+            search.setProductName(builder.toString());
             products.add(search);
         }
         return products;
